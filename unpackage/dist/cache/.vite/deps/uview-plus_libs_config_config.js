594ff93edcf713b45c5171e55077a227ec207c6b
sunshine995 <1982950647@qq.xom>
2025-11-18 08:15:31 +0800

    stop tracking node_modules

--- unpackage/dist/cache/.vite/deps/uview-plus_libs_config_config.js
+++
@@ -1,8 +0,0 @@
-import {
-  config_default
-} from "./chunk-I57R4HD3.js";
-import "./chunk-LQ2VYIYD.js";
-export {
-  config_default as default
-};
-//# sourceMappingURL=uview-plus_libs_config_config.js.map
